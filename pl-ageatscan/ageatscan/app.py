c739996ffdf1e7952f967bce381770540b7a3b12
Yanni Pang <yanni@bu.edu>
2021-10-08 00:04:41 -0400

    added descriptors

--- pl-ageatscan/ageatscan/app.py
+++ pl-ageatscan/ageatscan/app.py
@@ -7,6 +7,21 @@ from tqdm import tqdm
 
 
 class AgeAtScan(ChrisApp):
+    """
+     blah blah
+     """
+    PACKAGE = __package__
+    TITLE = 'Age Calculation'
+    CATEGORY = 'Math'
+    TYPE = 'ds'
+    ICON = ''  # url of an icon image
+    MIN_NUMBER_OF_WORKERS = 1  # Override with the minimum number of workers as int
+    MAX_NUMBER_OF_WORKERS = 1  # Override with the maximum number of workers as int
+    MIN_CPU_LIMIT = 1000  # Override with millicore value as int (1000 millicores == 1 CPU core)
+    MIN_MEMORY_LIMIT = 200  # Override with memory MegaByte (MB) limit as int
+    MIN_GPU_LIMIT = 0  # Override with the minimum number of GPUs as int
+    MAX_GPU_LIMIT = 0  # Override with the maximum number of GPUs as int
+
     def define_parameters(self):
         self.add_argument(
             '-p', '--inputPathFilter',
