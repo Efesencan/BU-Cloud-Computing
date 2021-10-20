a35e2e7cab1d6254e511696af0e9dcfae6d67af8
Yanni Pang <yanni@bu.edu>
2021-10-20 18:50:34 -0400

    changed cpus, gpus, cost_usd model to include a default value

--- chris-ultron-backend/chris_backend/plugins/models.py
+++ chris-ultron-backend/chris_backend/plugins/models.py
@@ -30,9 +30,9 @@ class ComputeResource(models.Model):
     name = models.CharField(max_length=100, unique=True)
     compute_url = models.URLField(max_length=300)
     description = models.CharField(max_length=600, blank=True)
-    cpus = models.IntegerField(blank=True)
-    gpus = models.IntegerField(blank=True)
-    cost_usd = models.FloatField(blank=True)
+    cpus = models.IntegerField(blank=True, default=0)
+    gpus = models.IntegerField(blank=True, default=0)
+    cost_usd = models.FloatField(blank=True, default=0.00)
 
     def __str__(self):
         return self.name
