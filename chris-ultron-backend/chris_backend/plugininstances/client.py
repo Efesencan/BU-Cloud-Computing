cedda3fedc8abb6c753a508e7bca57746c491dee
ronnakornRat <70792067+ronnakornRat@users.noreply.github.com>
2021-12-07 00:33:33 -0500

    Update client.py

--- chris-ultron-backend/chris_backend/plugininstances/client.py
+++ chris-ultron-backend/chris_backend/plugininstances/client.py
@@ -290,7 +290,7 @@ class ChrisClient:
         pass_count = 0
         pass_list = []
         for resource in compute_resources:
-            if resource['name'] in ['auto', 'auto_free', 'auto_budget']:
+            if resource['name'] in ['auto_free', 'auto_best']:
                 # don't check against 'auto' option
                 continue
             cmp_cpu = resource['cpus']
